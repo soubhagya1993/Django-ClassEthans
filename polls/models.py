112e3af3fbde47068e851ee16f8247ee2f0288c9
Soubhagya1993 <mohapatra.soubhagya@gmail.com>
2023-12-03 18:26:23 +0530

    added polls app

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=255)
+    pub_date = models.DateTimeField(default=timezone.now())
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
 # Create your models here.
